c15dce2328e7a5a41034b21015cb06706634f4e0
jan <janmasrovira@gmail.com>
2020-05-01 12:43:43 +0200

    update texclean

--- .config/fish/jan-config.fish
+++ .config/fish/jan-config.fish
@@ -44,7 +44,8 @@ abbr -a getaudio "youtube-dl --extract-audio --audio-quality 9" #download audio
 
 abbr -a gitzip "git archive HEAD --format=zip > repo.zip"
 
-abbr -a texclean "rm -rf auto/ *.log *.toc *.aux *.fls *.bcf *.gls *.ist *.run.xml *.glg *.blg *.fdb_latexmk _minted*"
+abbr -a texclean "rm -rf auto/ *.log *.toc *.aux *.fls *.bcf *.gls *.ist
+    *.run.xml *.glg *.blg *.fdb_latexmk _minted* *.out *.pyg *.glo *.synctex.gz"
 
 set --export TERM xterm-256color
 
