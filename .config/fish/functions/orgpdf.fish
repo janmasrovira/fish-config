7b31fa0b8a14bc0c9642b18fec36ab6dde39e574
Jan Mas Rovira <janmasrovira@gmail.com>
2020-10-31 19:58:37 +0100

    rename

--- .config/fish/functions/orgpdf.fish
+++ .config/fish/functions/orgpdf.fish
@@ -3,10 +3,11 @@
 # it splits horizontally the current tmux pane into 2 panes.
 # the top pane runs orgpvc and the bottom pane runs latexmk.
 
-# use: cd /report && orgtex report
-function orgtex
+# use: cd /report && orgpdf report
+function orgpdf
     set cmd "latexmk -pvc $argv[1]"
     set newpane (tmux split-window -v -P -F "#{pane_id}")
+    sleep 0.1 # needed so fish has time to start in the new pane.
     tmux send-keys -t $newpane $cmd C-m
     orgpvc $argv[1]
 end
