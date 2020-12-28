53c7c34c4928618c750a0c84396a3213fd5775a0
Jan Mas Rovira <janmasrovira@gmail.com>
2020-12-28 20:35:05 +0100

    .

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,10 +1,10 @@
 # Path to Oh My Fish install.
-set -q XDG_DATA_HOME
-  and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
-  or set -gx OMF_PATH "$HOME/.local/share/omf"
+# set -q XDG_DATA_HOME
+#   and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
+#   or set -gx OMF_PATH "$HOME/.local/share/omf"
 
 # load my personal config
 source ~/.config/fish/jan-config.fish
 
 # Load oh-my-fish configuration.
-source $OMF_PATH/init.fish
+# source $OMF_PATH/init.fish
