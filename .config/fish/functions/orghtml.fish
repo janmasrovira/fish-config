c1ee744dfca98bc0a3265388997d0f1fc733b8bf
Jan Mas Rovira <janmasrovira@gmail.com>
2021-01-19 15:31:42 +0100

    .

---
+++ .config/fish/functions/orghtml.fish
@@ -0,0 +1,11 @@
+#!/usr/local/bin/fish
+
+# use: orghtml orgfile (without extension)
+function orghtml
+    echo "watching " $argv[1].org
+    while inotifywait -e close_write $argv[1].org
+        set cmd "time emacs $argv[1].org -batch -f org-html-export-to-html"
+        echo $cmd
+        eval $cmd
+    end
+end
