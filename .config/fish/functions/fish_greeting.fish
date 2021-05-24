8617ca0c495197545076ed2d2d839044e77ff8d8
Jan Mas Rovira <janmasrovira@gmail.com>
2021-05-25 00:33:20 +0200

    .

--- .config/fish/functions/fish_greeting.fish
+++ .config/fish/functions/fish_greeting.fish
@@ -1,4 +1,4 @@
 function fish_greeting
-  date
+  date +"%a %b %d %R:%S %Z %Y" # same as default but 24h format instead of pm/am
   echo "My pid is" %self
 end
--- .config/fish/functions/orgpvc-generic.fish
+++ .config/fish/functions/orgpvc-generic.fish
@@ -4,13 +4,11 @@
 function orgpvc-generic --argument-names backend target
     echo "watching " $target.org
     while inotifywait -e close_write $target.org
-        docker create -ti --rm --name orgpvc janmasrovira/emacs-org:27.1
+        docker create -ti --rm --name orgpvc -v (pwd):/home/work janmasrovira/emacs-org:27.1
         docker start orgpvc
-        docker cp . orgpvc:/home/
-        set cmd "export EMACSLOADPATH=\$(cat /root/load-path) && cd /home/ && emacs $target.org -batch -l ~/init.el -f load-init-block -f org-$backend-export-to-latex"
+        set cmd "export EMACSLOADPATH=\$(cat /root/load-path) && cd /home/work && emacs $target.org -batch -l ~/init.el -f load-init-block -f org-$backend-export-to-latex"
         echo $cmd
         docker exec orgpvc sh -c $cmd
-        docker cp orgpvc:/home/$target.tex .
         docker kill orgpvc
     end
 end
