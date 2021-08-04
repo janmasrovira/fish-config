8c8651a3cf41a9c68ab74b53c6ca500040c60214
Jan Mas Rovira <janmasrovira@gmail.com>
2021-08-04 12:13:01 +0200

    improve keybindings

--- .config/fish/functions/jan_key_bindings.fish
+++ .config/fish/functions/jan_key_bindings.fish
@@ -1,11 +1,15 @@
+# hint: use fish_key_reader to get the code of a keybinding
 function jan_key_bindings
     fish_vi_key_bindings
-    bind --mode insert --sets-mode default fd backward-char force-repaint
+    bind --mode insert --sets-mode default fd backward-char repaint
 
     bind --mode insert \ck history-search-backward
     bind --mode insert \cj history-search-forward
 
+    bind --mode visual --sets-mode default \cG end-selection repaint
+    bind --mode visual --sets-mode default fd end-selection repaint
+
     for mode in insert default
-        bind --mode $mode \cf forward-char
+        bind --mode $mode \cf accept-autosuggestion
     end
 end
