0ba7c5077f66d23d5ddb6ef1c83ed019034200dc
Jan Mas Rovira <janmasrovira@gmail.com>
2024-04-24 19:47:32 +0200

    add completions to git

---
+++ .config/fish/completions/juvix.fish
@@ -0,0 +1,19 @@
+ function _juvix
+    set -l cl (commandline --tokenize --current-process)
+    # Hack around fish issue #3934
+    set -l cn (commandline --tokenize --cut-at-cursor --current-process)
+    set -l cn (count $cn)
+    set -l tmpline --bash-completion-enriched --bash-completion-index $cn
+    for arg in $cl
+      set tmpline $tmpline --bash-completion-word $arg
+    end
+    for opt in (juvix $tmpline)
+      if test -d $opt
+        echo -E "$opt/"
+      else
+        echo -E "$opt"
+      end
+    end
+end
+
+complete --no-files --command juvix --arguments '(_juvix)'
