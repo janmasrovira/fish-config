df8d64d516d1c99fbc8c2d684d0789b417ae748f
Jan Mas Rovira <janmasrovira@gmail.com>
2025-06-02 22:58:59 +0200

    chip-8 completions

---
+++ .config/fish/completions/chip-8.fish
@@ -0,0 +1,36 @@
+# Print an optspec for argparse to handle cmd's options that are independent of any subcommand.
+function __fish_chip_8_global_optspecs
+	string join \n h/help V/version
+end
+
+function __fish_chip_8_needs_command
+	# Figure out if the current invocation already has a command.
+	set -l cmd (commandline -opc)
+	set -e cmd[1]
+	argparse -s (__fish_chip_8_global_optspecs) -- $cmd 2>/dev/null
+	or return
+	if set -q argv[1]
+		# Also print the command, so this can be used to figure out what it is.
+		echo $argv[1]
+		return 1
+	end
+	return 0
+end
+
+function __fish_chip_8_using_subcommand
+	set -l cmd (__fish_chip_8_needs_command)
+	test -z "$cmd"
+	and return 1
+	contains -- $cmd[1] $argv
+end
+
+complete -c chip-8 -n "__fish_chip_8_needs_command" -s h -l help -d 'Print help'
+complete -c chip-8 -n "__fish_chip_8_needs_command" -s V -l version -d 'Print version'
+complete -c chip-8 -n "__fish_chip_8_needs_command" -f -a "completions" -d 'Generate shell completions'
+complete -c chip-8 -n "__fish_chip_8_needs_command" -f -a "run" -d 'Run the CHIP-8 emulator'
+complete -c chip-8 -n "__fish_chip_8_needs_command" -f -a "help" -d 'Print this message or the help of the given subcommand(s)'
+complete -c chip-8 -n "__fish_chip_8_using_subcommand completions" -s h -l help -d 'Print help'
+complete -c chip-8 -n "__fish_chip_8_using_subcommand run" -s h -l help -d 'Print help'
+complete -c chip-8 -n "__fish_chip_8_using_subcommand help; and not __fish_seen_subcommand_from completions run help" -f -a "completions" -d 'Generate shell completions'
+complete -c chip-8 -n "__fish_chip_8_using_subcommand help; and not __fish_seen_subcommand_from completions run help" -f -a "run" -d 'Run the CHIP-8 emulator'
+complete -c chip-8 -n "__fish_chip_8_using_subcommand help; and not __fish_seen_subcommand_from completions run help" -f -a "help" -d 'Print this message or the help of the given subcommand(s)'
