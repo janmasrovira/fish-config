628d6a9a13dc353ac28b4b8f4e6a379689810cc6
Jan Mas Rovira <janmasrovira@gmail.com>
2025-09-04 10:45:46 +0200

    remove orgpdf since I'm not using tmux anymore

--- .config/fish/functions/orgpdf.fish
+++
@@ -1,7 +0,0 @@
-# it splits horizontally the current tmux pane into 2 panes.
-# the top pane runs orgpvc and the bottom pane runs latexmk.
-
-# use: cd /report && orgpdf report
-function orgpdf --argument-names target
-    orgpdf-generic latex $target
-end
