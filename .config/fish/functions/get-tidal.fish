da782a605bf0b8c586038a48be043a0f3798851a
Jan Mas Rovira <janmasrovira@gmail.com>
2025-09-17 12:44:43 +0200

    get-tidal

---
+++ .config/fish/functions/get-tidal.fish
@@ -0,0 +1,8 @@
+# use: get-tidal https://tidal.com/album/198596141
+# downloads the album from tidal and copies it into the local beets library
+function get-tidal --argument-names album
+    set path ~/Downloads/get-tidal
+    tidal-dl -o $path -l "$album" -q HiFi -r P1080
+    beet import $path
+    mpc update --wait
+end
