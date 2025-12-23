69652aaa0c508fd115f4bfa1b148c8798c60399b
Jan Mas Rovira <janmasrovira@gmail.com>
2025-12-23 15:40:23 +0100

    use tidal-dl-ng

--- .config/fish/functions/get-tidal.fish
+++ .config/fish/functions/get-tidal.fish
@@ -2,7 +2,7 @@
 # downloads the album from tidal and copies it into the local beets library
 function get-tidal --argument-names album
     set path ~/Downloads/get-tidal
-    tidal-dl -o $path -l "$album" -q HiFi -r P1080
+    tidal-dl-ng dl "$album"
     beet import $path
     echo "updating mpc library... (safe to interrupt)"
     mpc update --wait
