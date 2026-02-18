8ab8659f1d7817dff752c461d644e3ba6eeeab4d
Jan Mas Rovira <janmasrovira@gmail.com>
2026-02-18 13:29:44 +0100

    use tiddl

--- .config/fish/functions/get-tidal.fish
+++ .config/fish/functions/get-tidal.fish
@@ -1,7 +1,9 @@
 # use: get-tidal https://tidal.com/album/198596141
 # downloads the album from tidal and copies it into the local beets library
 function get-tidal --argument-names album
-    set path ~/Downloads/get-tidal
-    tidal-dl-ng dl "$album"
+    set path ~/Downloads/tiddl
+    rm -rf $path
+    mkdir -p $path
+    tiddl download url "$album"
     beet import $path
 end
