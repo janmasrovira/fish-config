6ced093e512e6908ada1997ba8e3b4e02dc08e10
Jan Mas Rovira <janmasrovira@gmail.com>
2026-02-18 11:23:54 +0100

    completions for tiddl

---
+++ .config/fish/completions/tiddl.fish
@@ -0,0 +1,1 @@
+complete --command tiddl --no-files --arguments "(env _TIDDL_COMPLETE=complete_fish _TYPER_COMPLETE_FISH_ACTION=get-args _TYPER_COMPLETE_ARGS=(commandline -cp) tiddl)" --condition "env _TIDDL_COMPLETE=complete_fish _TYPER_COMPLETE_FISH_ACTION=is-args _TYPER_COMPLETE_ARGS=(commandline -cp) tiddl"
