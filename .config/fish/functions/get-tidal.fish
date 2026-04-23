6a287be2f8c7ebc97d1cbc9650bb43208aa9e7d9
Jan Mas Rovira <janmasrovira@gmail.com>
2026-04-23 09:24:34 +0200

    improve get-tidal

--- .config/fish/functions/get-tidal.fish
+++ .config/fish/functions/get-tidal.fish
@@ -1,8 +1,32 @@
-# use: get-tidal https://tidal.com/album/198596141
-# downloads the album from tidal and copies it into the local beets library
-function get-tidal --argument-names album
-    set path (mktemp -d); or return 1
-    trap "rm -rf $path" EXIT
-    tiddl download  --path $path url "$album"
-    beet import $path
+function get-tidal -d "Download Tidal Album via tiddl and import into beets"
+    if test (count $argv) -eq 0
+        echo "usage: get-tidal <tidal-url>" >&2
+        return 2
+    end
+    set -l album $argv[1]
+
+    if not command -q tiddl
+        echo "get-tidal: 'tiddl' not found in PATH" >&2
+        return 1
+    end
+    if not command -q beet
+        echo "get-tidal: 'beet' not found in PATH" >&2
+        return 1
+    end
+
+    if not mountpoint -q ~/music-beets
+        echo "get-tidal: ~/music-beets is not mounted" >&2
+        echo "  try: sshfs bee:/mnt/big-storage/music ~/music-beets" >&2
+        return 1
+    end
+
+    set -l tmpdir (mktemp -d); or return 1
+    tiddl download --path "$tmpdir" url "$album"; or begin
+        rm -rf "$tmpdir"
+        return 1
+    end
+    beet import "$tmpdir"
+    set -l rc $status
+    rm -rf "$tmpdir"
+    return $rc
 end
