e063a5ea3dd449b2bf7101594b81ffa92b5b97a0
Jan Mas Rovira <janmasrovira@gmail.com>
2026-06-24 12:06:10 +0200

    fix orgpvc

--- .config/fish/functions/orgpvc-generic.fish
+++ .config/fish/functions/orgpvc-generic.fish
@@ -3,7 +3,9 @@ function orgpvc-generic --argument-names backend target
     echo "watching " $target.org
     set dockerversion "2025-08-01"
     echo "using docker emacs-org:$dockerversion"
-    while inotifywait -e close_write $target.org
+    # watch the dir, not the file, so atomic saves (rename) don't drop the watch
+    inotifywait -q -m -e close_write -e moved_to --format '%f' (pwd) | while read -l changed
+        test "$changed" = "$target.org"; or continue
         docker create -ti --rm --name orgpvc -v (pwd):/home/work janmasrovira/emacs-org:$dockerversion
         docker start orgpvc
         set cmd "export EMACSLOADPATH=\$(cat /root/load-path) && cd /home/work && emacs $target.org -batch -l ~/init.el -f load-init-block -f org-$backend-export-to-latex"
